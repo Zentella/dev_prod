7111f438785765e4c3f336ef0a6c81816512cc9e
mauricio <mauricio.vera.r7@gmail.com>
2021-10-15 19:57:42 -0300

    v.2

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<Cuadro/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Cuadro from './components/Cuadro.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {Cuadro},
+};
+
 </script>
 
 <style>
---
+++ src/components/Cuadro.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="columnas">
+    <div id="box1" v-on:click="box1"></div>
+    <div
+      id="box">
+	</div>
+    <div id="box2" v-on:click="box2"></div>
+    <div id="box3" @click="box3"></div>
+    <div id="box4" @click="box4"></div>
+    <div id="box5" @click="box5"></div>
+    <div id="box6" v-on:click="box6"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Cuadro',  
+  methods: {
+	box1: function (){
+		document
+    .getElementById("box")
+    .setAttribute("style", "width: 300px;height:300px;background-color: red;");
+	},
+	box2: function (){
+		document
+    .getElementById("box")
+    .setAttribute(
+      "style",
+      "background: orange;"
+    );
+	},
+  box3: function (){
+		document
+    .getElementById("box")
+    .setAttribute("style", "width: 300px;height:300px;background-color: yellow;");
+	},
+  box4: function (){
+		document
+    .getElementById("box")
+    .setAttribute("style", "width: 300px;height:300px;background-color: green;");
+	},
+  box5: function (){
+		document
+    .getElementById("box")
+    .setAttribute("style", "width: 300px;height:300px;background-color: rgb(52, 201, 221);");
+	},
+  box6: function (){
+		document
+    .getElementById("box")
+    .setAttribute("style", "width: 300px;height:300px;background-color: rgb(187, 40, 118);");
+	},
+  }
+}
+
+</script>
+
+
+<style scoped>
+.columnas {
+  display: grid;
+  grid-template-columns: 50px 300px;
+  grid-template-rows: 50px 50px 50px 50px 50px 50px;
+}
+#box {
+  width: 300px;
+  height: 300px;
+  background-color: red;
+  border: 1px solid black;
+}
+#box1 {
+  width: 50px;
+  grid-column: 1 / 2;
+  height: 50px;
+  background-color: red;
+  border: 1px solid black;
+}
+#box2 {
+    width: 50px;
+    grid-column: 1 / 2;
+    height:50px;
+    background-color:orange;
+    border:1px solid black;
+}
+#box3 {
+  width: 50px;
+  grid-column: 1 / 2;
+  height: 50px;
+  background-color: yellow;
+  border: 1px solid black;
+}
+#box4 {
+    width: 50px;
+    grid-column: 1 / 2;
+    height:50px;
+    background-color:green;
+    border:1px solid black;
+}
+#box5 {
+  width: 50px;
+  grid-column: 1 / 2;
+  height: 50px;
+  background-color: rgb(52, 201, 221);
+  border: 1px solid black;
+}
+#box6 {
+    width: 50px;
+    grid-column: 1 / 2;
+    height:50px;
+    background-color:rgb(187, 40, 118);
+    border:1px solid black;
+}
+</style>
